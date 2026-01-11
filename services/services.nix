7abfd5b65c59daf72be2095666332cc25e97da6e
joshua <joshua-codes@proton.me>
2026-01-11 08:52:24 -0800

    add some services

--- services/services.nix
+++ services/services.nix
@@ -1,12 +1,14 @@
 # services.nix
 # imported by ../configuration.nix
-{ ... }:
-
-{
+_: {
 	imports = [
 		./ssh.nix
 		./pihole.nix
+		./traefik.nix
+		./dashy.nix
 		#./cryptpad.nix
+		#./nextcloud.nix
+		# ./immich.nix
 	];
 
 }
