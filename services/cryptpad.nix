0051eb8a190a704039e610b033050e6ae8eeae6d
joshua <joshua-codes@proton.me>
2026-01-11 08:53:21 -0800

    configure cryptpad

--- services/cryptpad.nix
+++ services/cryptpad.nix
@@ -1,13 +1,52 @@
-_: {
-
-	# non-docker attempt
-	services.cryptpad = {
-		enable = true;
-		settings = {
-			httpUnsafeOrigin = "https://cryptpad-ui.example.com";
+# cryptpad.nix
+# imported by ./services.nix
+_: 
+{
+	virtualisation.oci-containers.containers.cryptpad = {
+		image = "cryptpad/cryptpad:latest";
+		environment = {
+			CPAD_CONF = "/cryptpad/config/config.js";
+			CPAD_MAIN_DOMAIN = "http://cryptpad.local";
+			CPAD_SANDBOX_DOMAIN = "http://sandbox-cryptpad.local";
+			CPAD_HTTP_UNSAFE_ORIGIN = "http://cryptpad.local";
+			CPAD_HTTP_UNSAFE_SANDBOX = "http://sandbox-cryptpad.local";
+			CPAD_INSTALL_ONLYOFFICE = "no";
 		};
+		volumes = [
+			"/mnt/storage/srv/cryptpad/blob:/cryptpad/blob"
+			"/mnt/storage/srv/cryptpad/block:/cryptpad/block"
+			"/mnt/storage/srv/cryptpad/data:/cryptpad/data"
+			"/mnt/storage/srv/cryptpad/datastore:/cryptpad/datastore"
+		];
+		extraOptions = [
+			"--network=web"
+			"--label=traefik.enable=true"
+
+			# Main domain routing
+			"--label=traefik.http.routers.cryptpad.rule=Host(`cryptpad.local`)"
+			"--label=traefik.http.routers.cryptpad.entrypoints=web"
+			"--label=traefik.http.routers.cryptpad.service=cryptpad"
+			"--label=traefik.http.services.cryptpad.loadbalancer.server.port=3000"
+
+			# Sandbox domain routing
+			"--label=traefik.http.routers.cryptpad-sandbox.rule=Host(`sandbox-cryptpad.local`)"
+			"--label=traefik.http.routers.cryptpad-sandbox.entrypoints=web"
+			"--label=traefik.http.routers.cryptpad-sandbox.service=cryptpad"
+
+			# Headers middleware
+			"--label=traefik.http.middlewares.cryptpad-headers.headers.customrequestheaders.X-Forwarded-Proto=http"
+			"--label=traefik.http.routers.cryptpad.middlewares=cryptpad-headers"
+			"--label=traefik.http.routers.cryptpad-sandbox.middlewares=cryptpad-headers"
+		];
+		dependsOn = [ "traefik" ];
 	};
 
-
-	# docker attempt
+	# Create persistent directories with correct permissions
+	systemd.tmpfiles.rules = [
+		"d /mnt/storage/srv/cryptpad 0755 root root -"
+		"d /mnt/storage/srv/cryptpad/blob 0755 4001 4001 -"
+		"d /mnt/storage/srv/cryptpad/block 0755 4001 4001 -"
+		"d /mnt/storage/srv/cryptpad/data 0755 4001 4001 -"
+		"d /mnt/storage/srv/cryptpad/datastore 0755 4001 4001 -"
+	];
 }
